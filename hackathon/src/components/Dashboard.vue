aee2a80afe56a130f789d474146d104196dadf79
Joris Willig <joris@Uptrends.com>
2022-01-21 14:08:32 +0100

    Basic setup

---
+++ hackathon/src/components/Dashboard.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="dashboard" ref="dashboardContainer">
+  <Tile v-for="tile of dashboardConfig.tiles" :key="tile.id"></Tile>
+</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { TileConfig } from "./Tile.vue";
+
+export interface DashboardConfig {
+  gridWidth: number; // Amount of squares
+  gridHeight: number;
+  gapsize: number;
+  tiles: TileConfig[];
+}
+
+@Component({
+  components: {},
+})
+export default class Dashboard extends Vue {
+  @Prop() dashboardConfig!: DashboardConfig;
+
+  private squareHeight = 0;
+  private squareWidth = 0;
+
+  private calculateGridSize() {
+    const container = (this.$refs.dashboardContainer as HTMLElement);
+
+    const totalWidth = container.clientWidth;
+    const totalHeight = container.clientHeight;
+
+    this.squareHeight = Math.floor(totalHeight / this.dashboardConfig.gridHeight);
+    this.squareWidth = Math.floor(totalWidth / this.dashboardConfig.gridWidth);
+  }
+
+  private mounted() {
+    this.calculateGridSize();
+  }
+
+  private calculateTilePosition() {
+    //TODO
+  }
+
+  private calculateTileSize() {
+    //TODO
+  }
+
+  private gridToPx() {
+    //TODO
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ hackathon/src/components/Tile.vue
@@ -0,0 +1,28 @@
+<template>
+<div></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+export interface TileConfig {
+  id: string;
+  width: number;
+  height: number;
+  xPos: number;
+  yPos: number;
+}
+
+@Component({
+  components: {},
+})
+export default class Tile extends Vue {
+  @Prop() resizeFunction!: () => {x: number, y: number, width: number, height: number}; // px values
+  @Prop() moveFunction!: () => {x: number, y: number}
+  @Prop() gridToPx!: () => number;
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
